c3ddc26139c31e8e953f1284f81cbb072dd12ad1
beejones <ronny.bjones@hotmail.com>
2020-05-05 16:28:11 +0000

    Refactoring the Iexpected model

--- lib/Options/IExpected.ts
+++ lib/Options/IExpected.ts
@@ -10,6 +10,115 @@ import { TokenType } from '../index';
  */
 export type CheckTokenProperty = (property: any) => boolean;
 
+
+/**
+ * Expected values for SIOP
+ */
+export interface IExpectedSiop {
+
+  /**
+   * The token type
+   */
+  type: TokenType,
+
+  /**
+   * Expected audience for the token type
+   */
+  audience?: string , 
+}
+
+/**
+ * Expected values for verifiable presentation
+ */
+export interface IExpectedVerifiablePresentation {
+
+  /**
+   * The token type
+   */
+  type: TokenType,
+
+
+  /**
+   * Expected audience DID for the token type
+   */
+  didAdience?: string , 
+}
+
+/**
+ * Expected values for verifiable credentials
+ */
+export interface IExpectedVerifiableCredential {
+
+  /**
+   * The token type
+   */
+  type: TokenType,
+
+  /**
+   * Expected issuers for the token type (iss). 
+   * Configuration url for id tokens. The actual issuer is found in the issuer property of the configuration data.
+   */
+  issuers?: string[],
+
+  /**
+   * Expected audience for the token type
+   */
+  audience?: string , 
+
+  /**
+   * Verifiable credentials will use contracts to define their type
+   */
+  contracts?: string[],
+}
+
+/**
+ * Expected values for self issued tokens
+ */
+export interface IExpectedSelfIssued {
+
+  /**
+   * The token type
+   */
+  type: TokenType,
+
+  /**
+   * Expected issuers for the token type (iss). 
+   * Configuration url for id tokens. The actual issuer is found in the issuer property of the configuration data.
+   */
+  issuers?: string[],
+
+  /**
+   * Expected audience for the token type
+   */
+  audience?: string , 
+
+  /**
+   * Verifiable credentials will use contracts to define their type
+   */
+  contracts?: string[],
+}
+
+/**
+ * Expected values for id tokens
+ */
+export interface IExpectedIdToken {
+
+  /**
+   * The token type
+   */
+  type: TokenType,
+
+  /**
+   * Configuration url for id tokens. The actual issuer is found in the issuer property of the configuration data.
+   */
+  configuration: string[],
+
+  /**
+   * Expected audience for the token type
+   */
+  audience?: string
+}
+
 export default interface IExpected {
   /**
    * The DID of the validator
