a37ad0f78551d3445ee7c2625ae97ff38d6a85e5
Ronny Bjones <ronnybj@microsoft.com>
2020-05-27 10:44:27 -0700

    remove import

--- tests/Validator.spec.ts
+++ tests/Validator.spec.ts
@@ -3,7 +3,6 @@ import { IssuanceHelpers } from './IssuanceHelpers';
 import TestSetup from './TestSetup';
 import ValidationQueue from '../lib/InputValidation/ValidationQueue';
 import { SiopTokenValidator, SelfIssuedTokenValidator } from '../lib/index';
-import SiopValidationSimulation from './SiopValidationSimulation';
 
 describe('Validator', () => {
   let setup: TestSetup;
