c70a21cef95ab82288c4f2d7b80295c922c4f435
Ronny Bjones <Ronny.Bjones@microsoft.com>
2020-10-01 14:26:46 +0000

    adding public key suites

--- lib/input_validation/LinkedDataCryptoSuite.ts
+++ lib/input_validation/LinkedDataCryptoSuite.ts
@@ -25,22 +25,58 @@ export class LinkedDataCryptoSuitePublicKey {
       return {
         kty: 'OKP',
         use: 'sig',
-        alg: 'EdDSA'
+        alg: 'EdDSA',
+        crv: 'ed25519',
+        x: LinkedDataCryptoSuitePublicKey.decodeBase58To64Url(rawPublicKey.publicKeyBase58)
+      }
+    },
+    Ed25519VerificationKey2018: (rawPublicKey: any): object => {
+      if (!rawPublicKey) {
+        throw new Error('Pass in the public key. Undefined.');
+      }
+
+      if (!rawPublicKey.publicKeyBase58) {
+        throw new Error('publicKeyBase58 not defined in the public key.');
+      }
+
+      return {
+        kty: 'OKP',
+        use: 'sig',
+        alg: 'EdDSA',
+        crv: 'ed25519',
         x: LinkedDataCryptoSuitePublicKey.decodeBase58To64Url(rawPublicKey.publicKeyBase58)
       }
     }
+
   };
 
-  public static create(rawPublicKey: object): LinkedDataCryptoSuitePublicKey {
+
+  /**
+   * Return a normalized public key from the DID document.
+   * @param rawPublicKey Public key object from the DID document
+   */
+  public static getPublicKey(rawPublicKey: any): any {
+    if (!rawPublicKey) {
+      throw new Error(`The passed in public key is not defined`);
+    }
+
+    const suiteType = rawPublicKey.type;
+    if (!suiteType) {
+      throw new Error(`The passed in public key has no type. ${JSON.stringify(rawPublicKey)}`);
+    }
+
+    const suite = LinkedDataCryptoSuitePublicKey.suites[suiteType];
+    if (!suite) {
+      throw new Error(`The suite with type: '${suiteType}' is not supported.`);
+    }
+
+    return suite(rawPublicKey);
   }
 
-  constructor(private _rawPublicKey){
-  }
-
-  public get rawPublicKey(): object {
-    return this._rawPublicKey;
-  }
-
+  /**
+   * Convert base58 to base64 url.
+   * @param data to convert.
+   */
   private static decodeBase58To64Url(data: string): string {
     return data;
   }
